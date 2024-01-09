f6dbf32a75fd445bcd71231257775de2331d7935
Mohit Kumar Suman <83830347+mohitkrsuman@users.noreply.github.com>
2024-01-09 16:02:24 +0530

    deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
           <Route path="/orders" element={<h1>Orders</h1>} />
           <Route path="/products" element={<h1>Products</h1>} />
           <Route path="/delivery" element={<h1>Delivery</h1>} />
           <Route path="/marketing" element={<h1>Marketing</h1>} />
           <Route path="/analytics" element={<h1>Analytics</h1>} />
-          <Route path="/payments" element={<Payment/>} />
+          <Route path="/" element={<Payment/>} />
           <Route path="/tools" element={<h1>Tools</h1>} />
           <Route path="/discounts" element={<h1>Discounts</h1>} />
           <Route path="/audience" element={<h1>Audience</h1>} />
